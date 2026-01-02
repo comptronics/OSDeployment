51fcc29b9bac229490094932298902549fc8a339
comptronics <admin@comptronics.net>
2026-01-02 15:26:10 -0800

    Update osdcloud_config.ps1

--- osdcloud_config.ps1
+++ osdcloud_config.ps1
@@ -1,11 +1,13 @@
+$ErrorActionPreference = 'Stop'
+
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
 
 #Restart from WinPE
 
-Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
 
 Start-Sleep -Seconds 20
 
